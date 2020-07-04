e9536b20a077d4b7dd73c1d1b50efa7d3d359cdb
dhritisaini <dhritisaini@gmail.com>
2020-07-04 17:56:41 +0530

    Group anagrams

---
+++ group anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string> > mp;
+        for(string s: strs)
+        {
+            string t=s;
+            sort(t.begin(),t.end());
+            mp[t].push_back(s);
+        }
+        vector<vector<string>> v;
+        for(auto p: mp)
+        {
+            v.push_back(p.second);
+        }
+         return v;       
+    }
+};
